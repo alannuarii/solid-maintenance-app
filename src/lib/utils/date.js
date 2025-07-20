5b39b9cd16884933be02fd7241d3c13a371689f3
alannuarii <alannuariii@gmail.com>
2025-07-20 09:19:06 +0800

    Update

--- src/lib/utils/date.js
+++ src/lib/utils/date.js
@@ -1,9 +1,19 @@
 import { DateTime } from 'luxon';
 
-export function convertTime(waktu, format = 3, useZone = false) {
-    // Jika useZone true, set zona waktu ke 'Asia/Makassar', kalau false gunakan default
-    const dt = useZone
-        ? DateTime.fromISO(waktu, { zone: 'Asia/Makassar' })
+const DEFAULT_ZONE = 'Asia/Makassar';
+
+function isMobile() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+export function convertTime(waktu, format = 3) {
+    let zone;
+    if (process.env.NODE_ENV !== 'production' && !isMobile()) {
+        zone = DEFAULT_ZONE;
+    }
+
+    const dt = zone
+        ? DateTime.fromISO(waktu, { zone })
         : DateTime.fromISO(waktu);
 
     switch (format) {
@@ -14,6 +24,6 @@ export function convertTime(waktu, format = 3, useZone = false) {
         case 3:
             return dt.setLocale('id').toFormat('cccc, d LLLL yyyy');
         default:
-            return dt.toISO();  // fallback
+            return dt.toISO();
     }
 }
