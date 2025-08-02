484df703f4d7592146675eb563a4340d6007c3d9
alannuarii <alannuariii@gmail.com>
2025-08-02 16:43:09 +0800

    Update

---
+++ src/routes/api/pm/rencana/index.js
@@ -0,0 +1,40 @@
+import { json } from "@solidjs/router";
+import { generatePMSchedule } from "~/lib/utils/pmSchedule"
+
+function getStartAndEndDate() {
+    const today = new Date();
+    const startDate = today.toISOString().slice(0, 10);
+
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    if (month === 12) {
+        year += 1;
+        month = 1;
+    } else {
+        month += 1;
+    }
+
+    const endDateObj = new Date(year, month + 1 - 1, 0);
+    const endDate = endDateObj.toISOString().slice(0, 10);
+
+    return [startDate, endDate];
+}
+
+export async function GET() {
+    try {
+        const response = await fetch("http://localhost:3000/api/servicehour");
+        const data = await response.json();
+
+        const [startDate, endDate] = getStartAndEndDate();
+        const schedules = generatePMSchedule(data, startDate, endDate);
+        const result = schedules.map((schedule) => ({
+            unit: schedule.extendedProps.unit,
+            pm: schedule.title.split(' ')[0],
+            waktu: schedule.start,
+        }));
+        return json(result);
+    } catch (error) {
+        console.error("Error saat GET data", error);
+        return json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
